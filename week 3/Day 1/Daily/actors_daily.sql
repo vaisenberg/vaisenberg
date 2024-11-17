d6f9f2e4e1ac78cc2dab87faa0a7d05f8d295c34
vaisenberg <vaisenberg@icloud.com>
2024-11-17 18:26:00 +0200

    dailyw3d1

---
+++ week 3/Day 1/Daily/actors_daily.sql
@@ -0,0 +1,23 @@
+-- CREATE TABLE actors (
+-- actor_id SERIAL PRIMARY KEY,
+-- first_name VARCHAR(50) NOT NULL,
+-- last_name VARCHAR(50) NOT NULL,
+-- age DATE NOT NULL,
+-- number_oscars SMALLINT NOT NULL)
+-- SELECT * FROM actors
+-- INSERT INTO actors (first_name, last_name, age,number_oscars)
+-- VALUES ('Matt', 'Damon', '08/10/1970', 5);
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('George','Clooney','06/05/1961', 2)
+-- SELECT * FROM actors
+-- INSERT INTO actors (first_name, last_name, age,number_oscars)
+-- VALUES ('Brad', 'Pitt', '08/10/1970', 3)
+-- SELECT * FROM actors
+-- INSERT INTO actors (first_name, last_name, age,number_oscars)
+-- VALUES ('Angelina', 'Jolie', '08/10/1970', 3)
+-- SELECT * FROM actors
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('Meril','Streep','22/06/1949', 3)
+-- SELECT * FROM actors
+-- SELECT COUNT(*) FROM actors
